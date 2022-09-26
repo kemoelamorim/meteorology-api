df124cb7edecfd655f230a9577d86dc821ada748
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2022-09-26 15:33:13 -0300

    up do projeto

---
+++ src/test/java/workbd/meteorology/api/MeteorologyApplicationTests.java
@@ -0,0 +1,13 @@
+package workbd.meteorology.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
